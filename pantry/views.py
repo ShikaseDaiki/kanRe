35378391bcd3b935922164cb76d8563f71a439b9
ShikaseDaiki <91351969+Shikase-dev@users.noreply.github.com>
2023-01-08 19:11:35 +0900

    Pantryのviewsを設定した。

--- pantry/views.py
+++ pantry/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from pantry import serializers
+from core.models import Pantry, IngredientType, Ingredient
+from rest_framework import authentication, permissions
 
-# Create your views here.
+class PantryViewSet(viewsets.ModelViewSet):
+    queryset = Pantry.objects.all()
+    serializer_class = serializers.Pantry
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id = self.request.user)
